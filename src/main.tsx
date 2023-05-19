2811ef4d80a7e80296d3dc62c175d04c4f704e49
余瑞 <pazzitul@163.com>
2023-05-19 17:47:17 +0800

    v0.1.9-beta.5

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { MantineProvider, Global } from '@mantine/core'
+import { Notifications } from "@mantine/notifications";
+
 import './samples/node-api'
 import './index.scss'
+
 // fonts
 import UbuntuRegular from './assets/fonts/Ubuntu-Regular.ttf'
 import UbuntuBold from './assets/fonts/Ubuntu-Bold.ttf'
@@ -47,6 +50,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     }}
   >
     <UbuntuFont />
+    <Notifications />
     <App />
   </MantineProvider>
 )
